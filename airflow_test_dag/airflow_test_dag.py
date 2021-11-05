1d19723d9b6ec2646d94a35656c874823581604f
keshava-consumatrix-01 <keshava-consumatrix-01@users.noreply.github.com>
2021-11-05 20:25:46 +0100

    added test dag file

---
+++ airflow_test_dag/airflow_test_dag.py
@@ -0,0 +1,17 @@
+import mysql.connector
+from datetime import datetime
+mydb = mysql.connector.connect(host="5.255.98.125 ",
+                               user = "keshava",
+                               password = 'Dhanam_7',
+                               database = 'test'
+                               )
+
+mycursor = mydb.cursor()
+
+date_check = str(datetime.now())
+
+sql = (f"INSERT INTO airflow_test (date) VALUES ('{date_check}');")
+print(sql)
+mycursor.execute(sql)
+mydb.commit()
+
